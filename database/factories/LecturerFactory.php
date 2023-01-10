23a41e0e840b85c2ca6634afd2fbee21fe409c53
frchandra <nismara.chandra@gmail.com>
2023-01-10 10:27:11 +0700

    creating project, create lecturer models and seeder

---
+++ database/factories/LecturerFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LecturerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'phone_num' => fake()->phoneNumber(),
+        ];
+    }
+}
