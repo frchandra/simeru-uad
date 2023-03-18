1696c344a81db46f8c19273e53c7ee4c8a91eb9a
frchandra <nismara.chandra@gmail.com>
2023-03-18 17:46:10 +0700

    populating room time helper

--- database/seeders/RoomTimeHelperSeeder.php
+++ database/seeders/RoomTimeHelperSeeder.php
@@ -15,20 +15,21 @@ class RoomTimeHelperSeeder extends Seeder
      */
     public function run()
     {
-        for($i=1; $i<=8; $i++){ //room
-            for($j=1; $j<=6; $j++){ //hari
-                for($k=1; $k<=12; $k++){ // sesi
-                    RoomTimeHelper::create([
-                        "room_id" => $i,
-                        "time_id" => (($j-1)*12)+$k,
-                        "academic_year_id" => 1,
-                        "is_occupied" => false,
-                        "is_possible" => false,
-                    ]);
+        for($h=1; $h<=2; $h++) {//academic year id
+            for($i=1; $i<=8; $i++){ //room
+                for($j=1; $j<=6; $j++){ //hari
+                    for($k=1; $k<=12; $k++){ // sesi
+                        RoomTimeHelper::create([
+                            "room_id" => $i,
+                            "time_id" => (($j-1)*12)+$k,
+                            "academic_year_id" => 1,
+                            "is_occupied" => false,
+                            "is_possible" => false,
+                        ]);
+                    }
+
                 }
-
             }
-
         }
     }
 }
